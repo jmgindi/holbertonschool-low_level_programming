05ca9fcf208c26ef8c9eead35d226878518e8119
jmgindi <jmgindi@gmail.com>
2018-09-12 20:32:46 +0000

    formatting changes

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,13 @@
 #include <stdio.h>
-int main()
+
+/**
+ * main - entry
+ * 
+ * return - always 0
+ */
+int main(void)
 {
   printf("with proper grammar, but the outcome is a piece of art,\n");
+
   return 0;
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,18 @@
 #include <stdio.h>
+
+/**
+ * main - entry
+ * 
+ * return - always 0
+ */
 main(int)
 {
-  printf("Size of a char: %d bytes\n", sizeof(char));
-  printf("Size of an int: %d bytes\n", sizeof(int));
-  printf("Size of a long int: %d bytes\n", sizeof(long));
-  printf("Size of a long long int: %d bytes\n", sizeof(long long));
+  printf("Size of a char: %lu bytes\n", sizeof(char));
+  printf("Size of an int: %lu bytes\n", sizeof(int));
+  printf("Size of a long int: %lu bytes\n", sizeof(long int));
+  printf("Size of a long long int: %lu bytes\n", sizeof(long long));
+  printf("Size of a float: %lu bytes\n", sizeof(float));
 
-  return 0;
+  return (0);
 }
 
