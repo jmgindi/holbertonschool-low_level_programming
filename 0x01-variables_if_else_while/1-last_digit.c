fd1db7fc1ef1e08a697b7800ed483618830fe2a6
jmgindi <jmgindi@gmail.com>
2018-09-13 21:48:12 +0000

    forgot some words

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -28,7 +28,7 @@ int main(void)
 	}
 	else
 	{
-		printf("Last digit of %d is %d and is less than 6\n", n, d);
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, d);
 	}
 
 	return (0);
