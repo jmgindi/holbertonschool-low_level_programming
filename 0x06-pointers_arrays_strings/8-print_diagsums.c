37639b8d475c0e03fe97b1f19784c8c78923bd8c
jmgindi <jmgindi@gmail.com>
2018-10-02 02:40:06 +0000

    not sure what's going on here, trying some edits

--- 0x06-pointers_arrays_strings/8-print_diagsums.c
+++ 0x06-pointers_arrays_strings/8-print_diagsums.c
@@ -13,19 +13,19 @@ void print_diagsums(int *a, int size)
 	int sumrl = 0;
 	int sumlr = 0;
 	int i1 = 0;
-	int i2 = 1;
+	int i2 = 0;
 
 	while (i1 < size)
 	{
-		s1 += a[(size + 1) * i1];
+		sumrl += a[(size + 1) * i1];
 		i1++;
 	}
 
 	while (i2 < size)
 	{
-		s2 += a[(size - 1) * i2];
+		sumlr += a[(size - 1) * i2];
 		i2++;
 	}
 
-	printf("%i, %i\n", s1, s2);
+	printf("%i, %i\n", sumrl, sumlr);
 }
