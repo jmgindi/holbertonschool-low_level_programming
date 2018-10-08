4d4c80c21f2cc7bd57901cafa83cfe5c9320d874
jmgindi <jmgindi@gmail.com>
2018-10-08 02:56:56 +0000

    added helper function

--- 0x09-argc_argv/4-add.c
+++ 0x09-argc_argv/4-add.c
@@ -3,6 +3,33 @@
 #include "holberton.h"
 
 /**
+ * numcheck - checks for digit-only arguments
+ * @s: string to check
+ *
+ * Return: -1 if string contains non-digit, 0 otherwise
+ */
+
+int numcheck(char *s)
+{
+	char digit[] = "0123456789";
+	int i, dig_i, r;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		r = 0;
+		for (dig_i = 0; dig_i < 11; dig_i++)
+		{
+			if (s[i] == digit[dig_i])
+				r++;
+		}
+		if (r == 0)
+			return (-1);
+	}
+
+	return (0);
+}
+
+/**
  * main - entry point
  * @argc: number of arguments
  * @argv: array containing arguments
@@ -16,13 +43,12 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		x = atoi(argv[i]);
-		if (argv[i][0] != '0' && x == 0)
+		if (numcheck(argv[i]) == -1)
 		{
 			printf("Error\n");
 			return (1);
 		}
-		sum += x;
+		sum += atoi(argv[i]);
 	}
 	printf("%d\n", sum);
 
