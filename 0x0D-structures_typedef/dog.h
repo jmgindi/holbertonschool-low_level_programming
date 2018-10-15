e3934cf0e8686b9e3e1bd4eec4965c503a25b878
jmgindi <jmgindi@gmail.com>
2018-10-15 22:41:16 +0000

    first upload of struct definition

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+
+/**
+ * struct dog - struct of info about a dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
