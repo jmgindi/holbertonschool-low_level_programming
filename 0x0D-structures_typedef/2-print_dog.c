4cec42728532bd71d505317e64f551e80d7ea5a5
jmgindi <jmgindi@gmail.com>
2018-10-16 00:25:25 +0000

    removed a check

--- 0x0D-structures_typedef/2-print_dog.c
+++ 0x0D-structures_typedef/2-print_dog.c
@@ -18,9 +18,6 @@ void print_dog(struct dog *d)
 	if ((*d).owner == NULL)
 		(*d).owner = "(nil)";
 
-	if ((*d).age <= 0)
-		printf("Name %s\nAge: (nil)\nOwner: %s\n", (*d).name, (*d).owner);
-
 	else
 		printf("Name: %s\nAge: %f\nOwner: %s\n", (*d).name, (*d).age, (*d).owner);
 }
