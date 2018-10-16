c82aa7714ca5f4c09fa9f8d9332794c1afe21a15
jmgindi <jmgindi@gmail.com>
2018-10-16 01:58:48 +0000

    fixed betty formatting

--- 0x0D-structures_typedef/4-new_dog.c
+++ 0x0D-structures_typedef/4-new_dog.c
@@ -27,11 +27,14 @@ char *_strcpy(char *dest, char *src)
  * @name: dog's name
  * @age: dog's age
  * @owner: dog's owner
+ *
+ * Return: pointer to new dog
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *ret = malloc(sizeof(struct dog));
+
 	if (ret == NULL)
 		return (NULL);
 
