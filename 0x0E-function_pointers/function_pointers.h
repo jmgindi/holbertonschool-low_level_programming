11f625b3b59d606bca442f2ac8dfce9f9d979f89
jmgindi <jmgindi@gmail.com>
2018-10-17 23:13:40 +0000

    fixed style in header

--- 0x0E-function_pointers/function_pointers.h
+++ 0x0E-function_pointers/function_pointers.h
@@ -1,7 +1,7 @@
 #ifndef __FUNCTION_POINTERS_H__
 #define __FUNCTION_POINTERS_H__
 
-typedef long unsigned int size_t;
+typedef unsigned long int size_t;
 
 void print_name(char *name, void (*f)(char *));
 void array_iterator(int *array, size_t size, void (*action)(int));
