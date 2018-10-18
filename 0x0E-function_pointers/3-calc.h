42ee8970ead205aefc21983f91d50308cd187e9c
jmgindi <jmgindi@gmail.com>
2018-10-18 02:04:18 +0000

    first draft of files for 3

---
+++ 0x0E-function_pointers/3-calc.h
@@ -0,0 +1,10 @@
+#ifndef __3_CALC_H__
+#define __3_CALC_H__
+
+int op_add(int a, int b);
+int op_sub(int a, int b);
+int op_mul(int a, int b);
+int op_div(int a, int b);
+int op_mod(int a, int b);
+
+#endif
---
+++ 0x0E-function_pointers/3-main.c
@@ -0,0 +1,15 @@
+#include "3-calc.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+	int i, k;
+	char *operator;
+
+	i = atoi(argv[1]);
+	operator = argv[2];
+	k = atoi(argv[3]);
+
+
+}
