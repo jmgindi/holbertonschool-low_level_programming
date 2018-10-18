6a25fe4aa833c8454e49e6218ad3f31ecce82e53
jmgindi <jmgindi@gmail.com>
2018-10-18 23:23:54 +0000

    forgot to commit one file

---
+++ 0x0E-function_pointers/3-get_op_func.c
@@ -0,0 +1,34 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * get_op_func - gets operator function to pass into main
+ * @s: string containing operator
+ *
+ * Return: pointer to the function that corresponds
+ * to the operator given as a parameter
+ */
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	if (s == NULL || s[1] != '\0')
+		return (NULL);
+
+	i = 0;
+
+	while (i < 5 && s[1] != '\0')
+		i++;
+
+	return (ops[i].f);
+}
