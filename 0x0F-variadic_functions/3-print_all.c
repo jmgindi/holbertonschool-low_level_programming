65f37bfe713a9be0da9e9775a919ef9f086a1231
jmgindi <jmgindi@gmail.com>
2018-10-21 21:25:52 +0000

    fixed some errors, figuring out others

--- 0x0F-variadic_functions/3-print_all.c
+++ 0x0F-variadic_functions/3-print_all.c
@@ -10,47 +10,66 @@
 
 void print_all(const char * const format, ...)
 {
-	int i;
+	int i, j;
 	va_list valist;	
-	type tlist {
-		{"c", print_c};
-		{"i", print_i};
-		{"f", print_fl};
-		{"s", print_s};
-		{NULL, NULL};
+
+	flist tlist[] = {
+		{"c", print_c},
+		{"i", print_i},
+		{"f", print_fl},
+		{"s", print_s},
+		{NULL, NULL},
 	};
 
 	if (format == NULL)
 		return;
 
 	va_start(valist, format);
+	i = 0;
 
-	while (tlist[i].ltype != NULL)
+	while (format[i] != '\0')
 	{
-		if (tlist[i].ltype[0] == tlist[0])
-			do something;
+		j = 0;
+		while (tlist[j].name != NULL)
+		{
+			if (tlist[j].ltype == format[i])
+			{
+				tlist[j].f(va_arg(valist, void *));
+			}
+			j++;
+		}
 		i++;
 	}
+
+	printf("\n");
 
 	va_end(valist);
 }
 
 void print_c(va_list flist)
 {
-	printf("%c", va_arg(flist, char);
+	printf("%c", va_arg(flist, int));
 }
 
 void print_i(va_list flist)
 {
-	printf("%d", va_arg(flist, int);
+	printf("%d", va_arg(flist, int));
 }
 
 void print_fl(va_list flist)
 {
-	printf("%f", va_arg(flist, float);
+	printf("%f", va_arg(flist, double));
 }
 
 void print_s(va_list flist)
 {
-	printf("%s", va_arg(flist, char *);
+	char *str;
+
+	str = va_arg(valist, char *);
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	printf("%s", va_arg(flist, char *));
 }
--- 0x0F-variadic_functions/variadic_functions.h
+++ 0x0F-variadic_functions/variadic_functions.h
@@ -1,6 +1,18 @@
 #ifndef __VARIADIC_FUNCTIONS_H__
 #define __VARIADIC_FUNCTIONS_H__
 
+typedef struct flist {
+	char *form;
+	int (*f)(va_list);
+} tlist;
+
 int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_c(va_list valist);
+void print_i(va_list valist);
+void print_fl(va_list valist);
+void print_s(va_list valist);
+void print_all(const char * const format, ...);
 
 #endif
