cccf8c33afd9dad78289f6efd6029e2aba943da1
jmgindi <jmgindi@gmail.com>
2018-10-30 23:02:40 +0000

    removed excess else statement

--- 0x12-more_singly_linked_lists/7-get_nodeint.c
+++ 0x12-more_singly_linked_lists/7-get_nodeint.c
@@ -18,8 +18,8 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 			return (NULL);
 		else if (head->next == NULL)
 			break;
-		else
-			head = head->next;
+
+		head = head->next;
 	}
 
 	return (head);
