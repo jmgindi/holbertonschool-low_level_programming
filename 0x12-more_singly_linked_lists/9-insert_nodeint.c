41d0e011a88ce34bc9a3c8caa8bbef85ce27e220
jmgindi <jmgindi@gmail.com>
2018-10-31 02:08:55 +0000

    updating header and first upload of 9 that compiles

---
+++ 0x12-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,40 @@
+#include "lists.h"
+
+/**
+ *
+ *
+ *
+ *
+ *
+ *
+ */
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *temp, *newlint;
+	unsigned int index;
+
+	if (*head == NULL)
+		return (NULL);
+
+	temp = *head;
+	newlint = malloc(sizeof(listint_t));
+
+	if (newlint == NULL)
+	{
+		free(newlint);
+		return (NULL);
+	}
+
+	for (index = 0; temp != NULL && index < idx; index++)
+		temp = temp->next;
+
+	if (index == idx)
+	{
+		newlint->n = n;
+		newlint->next = temp->next;
+		temp->next = newlint;
+	}
+
+	return (newlint);
+}
