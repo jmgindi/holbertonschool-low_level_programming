892ac87dd0576c17c7ed90a96f18530a8249a573
jmgindi <jmgindi@gmail.com>
2018-11-06 22:05:50 +0000

    removed extraneous check on close

--- 0x14-file_io/0-read_textfile.c
+++ 0x14-file_io/0-read_textfile.c
@@ -11,38 +11,39 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	ssize_t rcount, wcount;
-	int fd, c;
+	int fd;
 	char *buf;
 
 	if (letters < 1 || filename == NULL)
 		return (0);
+
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
+
 	buf = malloc(letters * sizeof(char));
 	if (buf == NULL)
 	{
 		free(buf);
 		return (0);
 	}
+
 	rcount = read(fd, buf, letters);
 	if (rcount == -1)
 	{
 		free(buf);
 		return (0);
 	}
+
 	wcount = write(STDOUT_FILENO, buf, rcount);
 	if (wcount == -1)
 	{
 		free(buf);
 		return (0);
 	}
-	c = close(fd);
-	if (c == -1)
-	{
-		free(buf);
-		return (0);
-	}
+
+	close(fd);
+	
 	free(buf);
 	return (wcount);
 }
