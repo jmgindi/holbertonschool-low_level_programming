e4dee9a6965beaafa657bc750b15d17b88b01529
jmgindi <jmgindi@gmail.com>
2018-11-06 21:59:15 +0000

    first upload 0, readme, header

---
+++ 0x14-file_io/0-read_textfile.c
@@ -0,0 +1,48 @@
+#include "holberton.h"
+
+/**
+ * read_textfile - prints a number of letters from a text file to STDOUT
+ * @filename: name of file to open
+ * @letters: number of letters to print
+ *
+ * Return: actual number of letters read and printed, or 0 on failure
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t rcount, wcount;
+	int fd, c;
+	char *buf;
+
+	if (letters < 1 || filename == NULL)
+		return (0);
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
+		return (0);
+	buf = malloc(letters * sizeof(char));
+	if (buf == NULL)
+	{
+		free(buf);
+		return (0);
+	}
+	rcount = read(fd, buf, letters);
+	if (rcount == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	wcount = write(STDOUT_FILENO, buf, rcount);
+	if (wcount == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	c = close(fd);
+	if (c == -1)
+	{
+		free(buf);
+		return (0);
+	}
+	free(buf);
+	return (wcount);
+}
---
+++ 0x14-file_io/README.md
@@ -0,0 +1,4 @@
+0 - reads a text file and prints it to POSIX std out
+1 - creates a file
+2 - appends text to the end of a file
+3 - copies the content of one file to another file
