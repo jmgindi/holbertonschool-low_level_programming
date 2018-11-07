96bc58befb05a0d07b59db23b764051fada70a43
jmgindi <jmgindi@gmail.com>
2018-11-07 00:18:09 +0000

    fixed incorrect use of commas

--- 0x14-file_io/1-create_file.c
+++ 0x14-file_io/1-create_file.c
@@ -21,7 +21,7 @@ int create_file(const char *filename, char *text_content)
 	for (i = 0; text_content[i] != 0; i++)
 		;
 
-	newfile = open(filename, O_RDWR, O_TRUNC, O_CREAT, 00600);
+	newfile = open(filename, O_RDWR | O_TRUNC | O_CREAT, 00600);
 	if (newfile == -1)
 		return (-1);
 
