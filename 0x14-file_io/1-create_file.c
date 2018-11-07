f86166bd17dd00747a25fde4c6e3272cdef43a88
jmgindi <jmgindi@gmail.com>
2018-11-07 00:06:26 +0000

    first upload 1, output looks ok

---
+++ 0x14-file_io/1-create_file.c
@@ -0,0 +1,31 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file containing a string
+ * @filename: file name
+ * @text_content: NULL terminated string to write to file
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int newfile, wcount, i;
+
+	if (filename == NULL)
+		return (-1);
+
+	for (i = 0; text_content[i] != 0; i++)
+		;
+
+	newfile = open(filename, O_RDWR, O_CREAT, 00600);
+	if (newfile == -1)
+		return (-1);
+
+	wcount = write(newfile, text_content, i);
+	if (wcount == -1)
+		return (-1);
+
+	close(newfile);
+	return (1);
+}
