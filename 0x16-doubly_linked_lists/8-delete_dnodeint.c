7ac382bb590d263577a597a8a8d486a18d8bf226
jmgindi <jmgindi@gmail.com>
2018-12-10 05:06:52 +0000

    adding placeholder file for 8

---
+++ 0x16-doubly_linked_lists/8-delete_dnodeint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * delete_dnodeint_at_index - deletes a node at index
+ * @head: pointer to list
+ * @index: index to delete node at
+ *
+ * Return: 1 on success, -1 on failure
+ */
+
+int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
+{
+	(void)head;
+	(void)index;
+	return (-1);
+}
