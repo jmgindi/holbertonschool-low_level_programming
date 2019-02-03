1c0432aadf3cd9353101d3ff91d242d5a422ab57
jmgindi <jmgindi@gmail.com>
2019-02-03 21:56:26 +0000

    adding files for output testing

---
+++ 0x1A-sorting_algorithms/0-bubble_sort.c
@@ -0,0 +1,27 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - sorts an array using bubble sort
+ * @array: array to sort
+ * @size: size of array
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t l, r;
+	int storage;
+	if (size < 2 || !array)
+		return;
+	for (l = 0; l < (size - 1); l++)
+	{
+		for (r = 0; r < (size - l - 1); r++)
+		{
+			if (array[r] > array[r + 1])
+			{
+				storage = array[r];
+				array[r] = array[r + 1];
+				array[r + 1] = storage;
+				print_array(array, size);
+			}
+		}
+	}
+}
