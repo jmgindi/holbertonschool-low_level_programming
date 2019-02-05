ee37ee9a1dda61b04606ba14cd23baa78bf167d3
ConnorBrereton <breretonconnor@gmail.com>
2019-02-05 00:47:44 +0000

    Version 1 of quicksort

--- 0x1A-sorting_algorithms/3-quick_sort.c
+++ 0x1A-sorting_algorithms/3-quick_sort.c
@@ -1,15 +1,23 @@
 #include "sort.h"
 
 /**
- * quick_sort: sorts an array of integers using the quick sort method
- * @array: array to sort
- * @size: size of array
+ * quick_sort: Sorts an array of integers using the quick sort method.
+ *
+ * @array: Array to sort
+ * @size: Size of array
+ *
+ * Return: none (void function)
  */
 void quick_sort(int *array, size_t size)
 {
 	size_t lo, hi, p_index;
+
+	if (!array || size < 2)
+		return;
+
 	lo = array[0];
 	hi = array[size - 1];
+
 	if (lo < hi)
 	{
 		p_index = partition(array, lo, hi);
@@ -20,17 +28,20 @@ void quick_sort(int *array, size_t size)
 }
 
 /**
+ * partition: Sets the pivot and alligns elements to the
+ * left of the pivot point in ascending order.
  *
+ * @lo: First index, moves to right.
+ * @hi: End of array, where partition is set initially.
  *
- *
- *
+ * Return: Next pivot point.
  */
 size_t partition(int *array, size_t lo, size_t hi)
 {
+	int swap;	
 	size_t j, i = lo - 1;
-	int swap;
 
-	for(j = lo; j <= hi; j++)
+	for (j = lo; j <= hi - 1; j++)
 	{
 		if (array[j] <= arr[hi])
 		{
