704b402904c1ab98b5a45ff37d8dee0df6be2c00
jmgindi <jmgindi@gmail.com>
2019-02-12 01:13:30 +0000

    added check for land on edge

--- 0x1B-makefiles/5-island_perimeter.py
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -14,7 +14,7 @@ def island_perimeter(grid):
     if (not grid or not grid[0] or
         type(grid) is not list or
             type(grid[0]) is not list):
-        return 0
+        return
 
     perim = 0
 
@@ -22,9 +22,9 @@ def island_perimeter(grid):
         for j in range(len(grid[0]) - 1):
             if grid[i][j] is 1:
                 perim += 4
-                if grid[i][j - 1] is 1:
+                if grid[i][j - 1] is 1 and j - 1 > 0:
                     perim -= 2
-                if grid[i - 1][j] is 1:
+                if grid[i - 1][j] is 1 and i - 1 > 0:
                     perim -= 2
 
     return perim
