c1d2116d88cfb1ac3cad69f600e7ddc50a523afa
jmgindi <jmgindi@gmail.com>
2019-02-12 00:48:47 +0000

    first upload 5

---
+++ 0x1B-makefiles/5-island_perimeter.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+module contains 1 function:
+    island_perimeter
+"""
+
+
+def island_perimeter(grid):
+    """ finds the perimeter of an island
+
+    Args:
+        grid (obj:`list`): must be a list of lists
+    """
+    if (not grid or not grid[0] or
+        type(grid) is not list or
+            type(grid[0]) is not list):
+        return 0
+
+    perim = 0
+
+    for i in range(len(grid) - 1):
+        for j in range(len(grid[0]) - 1):
+            if grid[i][j] is 1:
+                perim += 4
+                if grid[i][j - 1] is 1:
+                    perim -= 2
+                if grid[i - 1][j] is 1:
+                    perim -= 2
+
+    return perim
