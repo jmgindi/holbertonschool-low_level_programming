8c16436112cb56d108d524ed7a1bb60b85a22051
jmgindi <jmgindi@gmail.com>
2019-03-15 18:03:14 +0000

    added null check

--- 0x1C-binary_trees/14-binary_tree_balance.c
+++ 0x1C-binary_trees/14-binary_tree_balance.c
@@ -36,6 +36,9 @@ int binary_tree_balance(const binary_tree_t *tree)
 {
 	int height_r, height_l;
 
+	if (!tree)
+		return (0);
+
 	height_r = binary_tree_height_int(tree->right);
 	height_l = binary_tree_height_int(tree->left);
 
