c5eefbcc72fd9b58af3dedfe94b9e0bb39b8785b
jmgindi <jmgindi@gmail.com>
2019-03-15 17:44:46 +0000

    added null check for func

--- 0x1C-binary_trees/6-binary_tree_preorder.c
+++ 0x1C-binary_trees/6-binary_tree_preorder.c
@@ -8,7 +8,7 @@
 
 void binary_tree_preorder(const binary_tree_t *tree, void(*func)(int))
 {
-	if (tree == NULL)
+	if (!tree || !func)
 		return;
 
 	func(tree->n);
--- 0x1C-binary_trees/8-binary_tree_postorder.c
+++ 0x1C-binary_trees/8-binary_tree_postorder.c
@@ -8,7 +8,7 @@
 
 void binary_tree_postorder(const binary_tree_t *tree, void(*func)(int))
 {
-	if (tree == NULL)
+	if (!tree || !func)
 		return;
 
 	binary_tree_postorder(tree->left, func);
