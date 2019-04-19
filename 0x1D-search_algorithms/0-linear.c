aa0adbd1aae08809a5909cbe8afb835a9d357c0e
jmgindi <jmgindi@gmail.com>
2019-04-19 00:42:11 +0000

    fixed betty and style issues

--- 0x1D-search_algorithms/0-linear.c
+++ 0x1D-search_algorithms/0-linear.c
@@ -10,13 +10,13 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
-	int i;
+	size_t i;
 
 	for (i = 0; i < size; i++)
 	{
-		printf("Value checked array[%d] = [%d]", i, array[i]);
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-			return (i);
+			return ((int) i);
 	}
-	return (-1)
+	return (-1);
 }
--- 0x1D-search_algorithms/1-binary.c
+++ 0x1D-search_algorithms/1-binary.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * binary_search - searches a sorted array using binary search
+ * @array: sorted array to search
+ * @size: length of array
+ * @value: value to look for
+ *
+ * Return: index of value, or -1 on failure
+ */
 int binary_search(int *array, size_t size, int value)
 {
 	int mid = size / 2, l = 0, r = size - 1;
@@ -13,7 +21,7 @@ int binary_search(int *array, size_t size, int value)
 		for (i = l; i <= r - 1; i++)
 			printf("%d, ", array[i]);
 		printf("%d\n", array[i]);
-		
+
 		mid = (l + r) / 2;
 		if (array[mid] == value)
 			return (mid);
